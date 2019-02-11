a7e44b46cd69c1ab5f6e83b16f1d3a6604f21069
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-02-11 14:11:59 -0500

    first final version of the Queue.py

--- Queue.py
+++ Queue.py
@@ -8,5 +8,17 @@ class Queue(LinkedList):
 
     def enqueue(self,valueOfNode):
         if self.isEmpty():
-            n = Node(valueOfNode)
-            
+            n = Node(valueOfNode,None)
+
+    def dequeue(self):
+        nodeToDequeue = self.head.nextNode
+        self.head.nextNode = self.head
+        return(nodeToDequeue)
+
+
+a = Queue()
+a.enqueue("Nicholas")
+a.enqueue("Mitchell")
+a.enqueue("laney")
+a.enqueue("Dane")
+a.enqueue("Sheila")
