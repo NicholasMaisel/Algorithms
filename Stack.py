ad00c1229ce3ae9c18ca6fac4acb6876c339ac94
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-02-11 13:43:59 -0500

    first version of the Stack.py file (untested)

--- Stack.py
+++ Stack.py
@@ -1,8 +1,42 @@
 from LinkedList import Node
 from LinkedList import LinkedList
 
-class Stack(LinkedList):
-    pass
+class Stack(LinkedList):        #this inherits the Node class
+    def __init__(self):
+        self.top = None
+        self.firstNode = None
 
-a = Stack(None)
-print(dir(a))
+    def isEmpty(self):
+        if self.firstNode:
+            empty = False
+        else:
+            empty = True
+        return(empty)
+
+    def push(self,valueOfNode):
+        if isEmpty():
+            n = Node(valueOfNode,None)
+            self.firstNode = n
+            self.top = n
+        else:
+            n = Node(valueOfNode,None)
+            self.top.nextNode = n
+            self.top = n
+
+    def TraverseToNewTop(self):
+        n = self.firstNode
+        while(n.nextNode != self.top):
+            n = n.nextNode
+        return(n)
+
+
+    def pop(self):
+        if not self.isEmpty():
+            poppedNode = N.top
+            self.top = TraverseToNewTop()
+            self.top.nextNode = None
+            return(poppedNode)
+
+
+a = Stack()
+a.push("")
