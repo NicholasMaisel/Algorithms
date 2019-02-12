5ce6f552c4c7a69086688323bf7f426585ed00b9
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-02-12 11:02:58 -0500

    Added some comments for readability, and also refined some small coding stylistic errors

--- LinkedList.py
+++ LinkedList.py
@@ -1,3 +1,6 @@
+
+#The Node class is used as the objects for each element in the Linked list
+# It keeps track of what value each node holds and what the nextNode in the list
 class Node():
     def __init__(self,valueOfNode,nextNode):
         self.valueOfNode = valueOfNode
@@ -7,10 +10,26 @@ class LinkedList():
     def __init__(self,firstNode):
         self.firstNode = firstNode
 
+    def isEmpty(self):
+        if self.firstNode:
+            empty = False
+        else:
+            empty = True
+        return(empty)
+
     def AddToFront(self,valueOfNode):
-        n = Node(valueOfNode, self.firstNode)
-        self.firstNode = n
-        n.valueOfNode = valueOfNode
+        if self.isEmpty():
+            n = Node(valueOfNode, None) #Creates new node to be added to list.
+            n.nextNode = self.firstNode #Adds node to the list, attaching at front.
+            self.firstNode = n          #Makes new node the first node
+            n.valueOfNode = valueOfNode #sets the value of the new node
+
+        else:
+            n = Node(valueOfNode, self.firstNode)
+            n.nextNode = self.firstNode
+            self.firstNode = n
+            n.valueOfNode = valueOfNode
+
 
     def TraverseToLastNode(self):   #This function is used to add nodes to the end in AddToEnd()
         n = self.firstNode
@@ -19,15 +38,28 @@ class LinkedList():
         return(n)
 
     def AddToEnd(self,valueOfNode):
-        last = self.TraverseToLastNode()    #This finds the last node so we can update nextNode of the last Node
-        n = Node(valueOfNode,None)
-        last.nextNode = n
-
-    def isEmpty(self):
-        if self.firstNode:
-            empty = False
+        if self.isEmpty():
+            n = Node(valueOfNode, None)
+            self.firstNode = n
+            n.valueOfNode = valueOfNode
         else:
-            empty = True
-        return(empty)
+            last = self.TraverseToLastNode()    #This finds the last node so we can update nextNode of the last Node
+            n = Node(valueOfNode,None)
+            last.nextNode = n
 
-f = open(sasda)
+
+
+f = open('/Users/nicholasmaisel/Documents/Programming/Algorithms/magicitems.py',"r")
+lines = list(f)
+f.close
+
+a = LinkedList(None)
+for i in lines:
+    a.AddToEnd(i)
+    print(i)
+
+print(a.firstNode.nextNode)
+
+while a.firstNode != None:
+    print('###',a.firstNode.valueOfNode)
+    a.firstNode = a.firstNode.nextNode
