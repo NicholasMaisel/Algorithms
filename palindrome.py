fdf8336e2f36c333396538f83c8da2730db645bc
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-02-12 14:05:07 -0500

    first tested version of the palindrome file

--- palindrome.py
+++ palindrome.py
@@ -2,26 +2,38 @@ from Queue import Queue
 from Stack import Stack
 
 def isPalindrome(magicitems):
-for item in magicitems:
-    item = item.strip()
-    item = item.lower()
-    palindrome = True
-    a = Queue()
-    b = Stack()
-    for letter in item:
-        a.enqueue(letter)
-        b.push(letter)
+    for item in magicitems:
+        item = item.strip()
+        item = item.replace(' ','')
+        item = item.lower()
 
-    while not b.isEmpty():
-        if a.dequeue() == b.pop():
-            pass
-        else:
-            palindrome = False
-            return(palindrome)
-        return(palindrome)
+        palindrome = True
+        a = Queue()
+        b = Stack()
+
+        for letter in item:
+            a.enqueue(letter)
+            b.push(letter)
+            palindrome = True
+
+        while not b.isEmpty() and not a.isEmpty():
+            x = b.pop().valueOfNode
+            w = a.dequeue().valueOfNode
+            if x == w:
+                pass
+            else:
+                palindrome = False
+        if palindrome:
+            print(item, " Plaindrome =", palindrome)
 
 
 
-f = open('/Users/nicholasmaisel/Documents/Programming/Algorithms/magicitems.py',"r")
+
+
+
+
+f = open('/Users/nicholasmaisel/Documents/Programming/Algorithms/magicitems.txt',"r")
 magicitems = list(f)
 f.close
+
+print(isPalindrome(magicitems))
