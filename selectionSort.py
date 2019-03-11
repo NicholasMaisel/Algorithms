68279573da653a68186c849e991b844ee35226cb
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-03-11 13:06:11 -0400

    adds comparison counter to the selection sort algorithm and its output.

--- selectionSort.py
+++ selectionSort.py
@@ -5,18 +5,29 @@ def swap(Arr, j, smallPosition):
     return(Arr)
 
 def selectionSort(A):
+    comparisons = 0     #used to count the number of comparisons
     n = len(A)
     j = 0
     for j in range(j, n-1):
         smallPos = j
         for x in range(j+1, n):
+            #increments comparisons each time one is done, it is before the
+            # inequality, because the inequality may not always be true.
+            comparisons += 1
             if A[x] < A[smallPos]:
                 smallPos = x
-        A = swap(A,j,smallPos)
-    return(A)
+        A = swap(A,j,smallPos)  # Calls the swap function
+    return(A,comparisons)       # Returns tuple of the sorted list and # of comps
+
+
+def mergeSort
 
 
 
+'''
+For the comparison count, I am getting 221,445 comparisons. We learned that
+selection sort operates at O(n^2). Am i counting all comparisons?
+Am i missing anything?'''
 
 
 
@@ -32,9 +43,10 @@ def main():
     magicitems = [x.replace(' ','') for x in magicitems]
 
     a = magicitems
-    a = selectionSort(a)
+    a,comparisonCount = selectionSort(a)
     for i in a:
         print(i)
+    print("Comparisons: ", comparisonCount)
 
 
 main()
