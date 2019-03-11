468e7b79228ee18da3b3e37c83f6b093e94b24c5
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-03-11 19:38:20 -0400

    Adds the search algorithms without the comparison counters

---
+++ searchAlgorithms.py
@@ -0,0 +1,56 @@
+from random import random
+
+def linearSearch(A, target):
+    location = 0
+    while location < len(A):
+        if A[location] == target:
+            flag = True
+            break
+        else:
+            flag = False
+        location +=1
+
+    if flag == False:
+        location = -1
+    return(flag,location)
+
+def binarySearch(A,start,stop,target):
+    flag = False
+    midpoint = int((start + stop)/2)
+    if start > stop:
+        flag = False
+        location = -1
+    elif (A[midpoint] == target):
+        flag = True
+        location = midpoint
+    elif (target < midpoint):
+        binarySearch(A,start, midpoint-1, target)
+    else:
+        binarySearch(A, midpoint+1,stop,target)
+
+    return(flag,midpoint)
+
+
+
+
+
+#Warning: this function is designed to work with searches that come back with a
+#true result, the search functions return [-1] if the item is not found.
+def main(A):
+    trials = 100
+    totalComparisonsLinear = 0
+    totalComparisonsBinary = 0
+    for i in range(trials):
+        randomItem = A[round(random()*len(A))-1]
+        totalComparisonsLinear += linearSearch(A,randomItem)[1]
+    print("Avg. comparisons (LinearSearch): ", totalComparisonsLinear/trials)
+
+    for i in range(42):
+        randomItem = A[round(random()*len(A))-1]
+        totalComparisonsBinary += binarySearch(A,0,len(A)/2,randomItem)[1]
+
+    print("Avg. comparisons (LinearSearch): ", totalComparisonsBinary/trials )
+
+
+A = [1,2,3,4,5,6,7,8,9]
+main(A)
