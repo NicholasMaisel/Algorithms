36f1882fd2789c07206098a463eba9d812d8791b
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-03-11 11:54:32 -0400

    Fixed a few indexing and variable issues. Also ran the first few tests to ensure the algorithm works when using integers as the type of the array

--- selectionSort.py
+++ selectionSort.py
@@ -7,11 +7,23 @@ def swap(Arr, j, smallPosition):
     return(Arr)
 
 
-def selectionSort(A)
+def selectionSort(A):
     n = len(A)
     j = 0
-    for i in range(A, j):
+    for j in range(j, n-2):
         smallPos = j
-        for i in range(j+1, n-1)
-            if A[i] < A[smallPos]
-    swap(A,j,smallPos)
+        for x in range(j+1, n):
+            if A[x] < A[smallPos]:
+                smallPos = x
+                print("smallPos = ",smallPos)
+        A = swap(A,j,smallPos)
+    return(A)
+
+
+def main():
+    a = [1,3,5,2,4,2,4553,234,2355,262,342,245234,2245,23523,52]
+    selectionSort(a)
+    print(a)
+
+
+main()
