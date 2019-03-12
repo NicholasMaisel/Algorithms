76dd8eed7f88675184f7155043e4f936f8d7ce61
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-03-11 20:16:09 -0400

    Adds the hash file and a few statements

---
+++ hashAlgorithm.py
@@ -0,0 +1,27 @@
+import LinkedList
+
+ARRAY_LENGTH = 255
+
+
+class HashTable():
+    def __init__(self,chainLength):
+        self.chainLength = chainLength
+        self.table = {}
+
+    def hash(key):
+        return(key%chainLength)
+
+    def get(key):
+        
+
+
+    def put(key):
+        if a.get(self.hash(key)):
+            a.get(self.hash(key)).AddToFront(key)
+
+
+
+
+
+def main():
+    myHashTable = HashTable(ARRAY_LENGTH)
