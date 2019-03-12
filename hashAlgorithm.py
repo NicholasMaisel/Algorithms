bfd254108781ec1d06259704930253f1aca1f01e
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-03-11 20:48:42 -0400

    progress with the put and get method stubs

--- hashAlgorithm.py
+++ hashAlgorithm.py
@@ -12,15 +12,37 @@ class HashTable():
         return(key%chainLength)
 
     def get(key):
-        
+        flag = False
+        hashedKey = self.hash(key)
+        #See if hash table has a value at hashed index
+        if hashedKey in self.table.values:
+            selectedLList  = self.table[self.hash(key)] #grabs the linked list to search for element.
+            if selectedLList.firstNode.valueOfNode == key:
+                return(key)
+            else:
+                curNode = selectedLList.firstNode.nextNode
+                # Runs thru the linked list checking if curNode.valueOfNode = key
+                while curNode.ValueOfNode != key:
+                    if curNode.nextNode: #If the current node has a next node, set CurNode equal to it
+                        curNode = curNode.nextNode
+                    else:
+                        break   #if there isnt a next node break out and move on
+                if curNode.valueOfNode == key: #ensures that the curNodes value is the value we are looking for
+                    flag = True
+        else:
+            flag = False
 
+        return(flag)
 
     def put(key):
-        if a.get(self.hash(key)):
-            a.get(self.hash(key)).AddToFront(key)
-
-
-
+        hashedKey = self.hash(key)
+        if hashedKey in self.table.values:
+            self.table[hashedKey].addToFront(key)
+        else:
+            #If there is not already a value mapped to this hash output's
+            #Linked list, simply make one and set the key to the value of that
+            #linked lists's firstNode value
+            selt.table[hashedKey] = LinkedList(key)
 
 
 def main():
