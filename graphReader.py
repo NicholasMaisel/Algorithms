7910b1ba405a97a84576fbfd8e861793233da9de
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-04-15 15:59:45 -0400

    First fully functional version of the graph reader

--- graphReader.py
+++ graphReader.py
@@ -10,29 +10,31 @@ def readFile():
 def follow(instructions):
     index = 0
     separateGraphCount = -1
-    graphAdj = [] #list of dictionaries representing each graph
+    graphAdj = [] #list of dictionaries, each dictionary representing each graph
     while index < len(instructions):
-        print(graphAdj)
-        print(instructions[index])
+        if len(instructions[index].strip()) == 0: # Checks for blank line
+            index +=1
+
         if instructions[index][:2] == '--':    # Ignores comments
             index += 1
 
         elif instructions[index][:3] == 'new':
-            graphAdj.append({})
-            separateGraphCount +=1      #updates which dictionary is holding current graph
-            index +=1
+            graphAdj.append({})         # Creates new dictionary indicating a separate graph
+            separateGraphCount +=1      # Updates which dictionary is holding current graph
+            index +=1                   # Moves to next instruction
 
         elif instructions[index][4:8] == 'vert':
             print(instructions[index][12:])
             graphAdj[separateGraphCount][instructions[index][11:].replace('\n','')] = []
+            #adds vertex as key in dictionary graphAdj
             index += 1
 
         elif instructions[index][4:8] == "edge":
             v1, v2 = instructions[index][9:].replace(' ','').split('-')
+            #adds value to list with corresponding key (number of vertex)
 
 
             print(graphAdj)
-            input()
             if v1 in graphAdj[separateGraphCount].keys():   #Checks if there is a key for that node
                 graphAdj[separateGraphCount][v1].append(v2) #Adds the second vertex of the edge
             else:
