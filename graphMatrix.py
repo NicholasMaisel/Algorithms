a62cae7c83398eea0eb2ee88ee22e8fffbe8677f
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-04-15 17:13:21 -0400

    adds functionality to the GraphMatrix class, it can now take adjacency dictionaries as inputs and convert them to matrix representations

--- graphMatrix.py
+++ graphMatrix.py
@@ -3,3 +3,19 @@ from vertex import Vertex
 class GraphMatrix:
     def __init__(self):
         self.matrix = []
+
+    def makeMatrix(self, adjacencyList):
+        ''' The adjacency list object should be a dictionry of verticies as keys,
+        with a list of attached verticies as its value'''
+
+        for i in range(len(adjacencyList)):
+            self.matrix.append([])    # Makes a list for each vertex
+            for x in range(len(adjacencyList)):
+                self.matrix[i].append(0) # Adds zeros to matrix
+
+        for vert in adjacencyList:  # Loops through each vertex
+            for connection in adjacencyList[vert]:  # Loops through each adj vertex
+                print("VERT, CONNECTION: ", vert, connection)
+                self.matrix[int(vert)-1][int(connection)-1] = 1  # Updates the matrix with a connection
+
+        print(self.matrix)
