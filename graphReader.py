0c2e203287be8aeca2bb507e4dd70b153efd7a9e
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-04-15 15:51:02 -0400

    Adds chunk of body to interpret inputs for graph

--- graphReader.py
+++ graphReader.py
@@ -1,25 +1,51 @@
+import vertex
+import Graph
+
 def readFile():
     with open('/Users/nicholasmaisel/Documents/Programming/Algorithms/graphs.txt','r') as graphsFile:
         graphs = graphsFile.readlines()
     return(graphs)
 
 
-def interpret(instructions,flag, instructions):
+def follow(instructions):
     index = 0
+    separateGraphCount = -1
+    graphAdj = [] #list of dictionaries representing each graph
     while index < len(instructions):
-        if instructions[index][:2] == "--":
-            pass
-        elif instructions[index][:3] == 'new':
-            while instructions[index][:3] != 'new':
-                #create graph based on graphType
-                
+        print(graphAdj)
+        print(instructions[index])
+        if instructions[index][:2] == '--':    # Ignores comments
+            index += 1
 
-        else:
-            print("Not Understood.\n\n")
+        elif instructions[index][:3] == 'new':
+            graphAdj.append({})
+            separateGraphCount +=1      #updates which dictionary is holding current graph
+            index +=1
+
+        elif instructions[index][4:8] == 'vert':
+            print(instructions[index][12:])
+            graphAdj[separateGraphCount][instructions[index][11:].replace('\n','')] = []
+            index += 1
+
+        elif instructions[index][4:8] == "edge":
+            v1, v2 = instructions[index][9:].replace(' ','').split('-')
+
+
+            print(graphAdj)
+            input()
+            if v1 in graphAdj[separateGraphCount].keys():   #Checks if there is a key for that node
+                graphAdj[separateGraphCount][v1].append(v2) #Adds the second vertex of the edge
+            else:
+                graphAdj[separateGraphCount][v1] = [v2]
+
+            index += 1
+    return(graphAdj)
+
 
 
 
 def main():
     instructions = readFile()
-    for graphType in ['m','a','l']:     # marks the type to be interpreted
-        interpret(instructions, graphType, instructions)
+    follow(instructions)
+
+main()
