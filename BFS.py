e69d45c18db5def9b343d624b7b8c8da28318a15
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-04-16 18:33:55 -0400

    Changes manual change of marked attribute to the use of a class method

--- BFS.py
+++ BFS.py
@@ -1,16 +1,16 @@
 from Queue import Queue
-def BFS(graphList, target):
+def Bfs(graphList, target):
     target = int(target)-1
     Q = Queue()
     Q.enqueue(graphList[target]) # Takes the first Vertex and adds it to Queue
-    graphList[target].marked = 1  # Marks target
+    graphList[target].mark()  # Marks target
     while Q.isEmpty != False:
         v = Q.dequeue()
         if v == None:
             break
         print(v.valueOfNode.vid)
-        v.valueOfNode.marked = True
+        v.valueOfNode.mark()
         for w in v.valueOfNode.adjList:
             if w.marked == False:
-                w.marked = True
+                w.mark()
                 Q.enqueue(w)
