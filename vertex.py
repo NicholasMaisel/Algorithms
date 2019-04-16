e882cdc82d7b20fa8b71a7c592ee2b0f23e34a67
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-04-16 12:11:33 -0400

    Fixes addEdge() function to work correctly

--- vertex.py
+++ vertex.py
@@ -2,4 +2,7 @@ class Vertex:
     def __init__(self,vid):
         self.vid = vid
         self.adjList = []
-        
+        self.marked = 0
+
+    def addEdge(self, vid):
+        self.adjList.append(vid) #adds vertex to the adjacency list
