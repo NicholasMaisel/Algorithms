abade6f07a0576e92df3bfae18b58af85b43a284
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-04-16 11:19:10 -0400

    Makes updates to the file to allow for proper representation of linkedObjects graph

--- graphReader.py
+++ graphReader.py
@@ -1,4 +1,4 @@
-import vertex
+from vertex import Vertex
 import Graph
 from graphMatrix import GraphMatrix
 def readFile():
@@ -23,17 +23,15 @@ def follow(instructions):
             index +=1                   # Moves to next instruction
 
         elif instructions[index][4:8] == 'vert':
-            print(instructions[index][12:])
+
             graphAdj[separateGraphCount][instructions[index][11:].replace('\n','')] = []
             # Adds vertex as key in dictionary graphAdj
             index += 1
 
         elif instructions[index][4:8] == "edge":
-            v1, v2 = instructions[index][9:].replace(' ','').split('-')
+            v1, v2 = instructions[index][9:].replace(' ','').replace('\n','').split('-')
             # Adds value to list with corresponding key (number of vertex)
 
-
-            print(graphAdj)
             if v1 in graphAdj[separateGraphCount].keys():   # Checks if there is a key for that node
                 graphAdj[separateGraphCount][v1].append(v2) # Adds the second vertex of the edge
             else:
@@ -44,11 +42,43 @@ def follow(instructions):
 
 def main():
     instructions = readFile()
-    adjList = follow(instructions)
+    MyAdjList = follow(instructions)
+
     matricies = []
-    for graph in range(len(adjList)):
+    linkedObjects = []
+
+
+
+    for graph in range(len(MyAdjList)):
+        linkedObjects.append([])
+
+        # Make graph matricies
         matricies.append(GraphMatrix())
-        matricies[graph].makeMatrix(adjList[graph])
+        matricies[graph].makeMatrix(MyAdjList[graph])
+
+
+        # Make connected objects
+        for i in range(len(MyAdjList[graph])):
+            linkedObjects[graph].append(0)
+            #Creates placeholders
+
+        for vert in MyAdjList[graph].keys():
+            linkedObjects[graph][int(vert)-1] = Vertex(vert)
+            # Creates Vertex objects and stores them in a list
+
+        for vert in MyAdjList[graph]:
+            # add corresponding verticies to adjList of Vertex
+            for edge in MyAdjList[graph][vert]:
+                linkedObjects[graph][int(vert)-1].addEdge(linkedObjects[graph][int(edge)-1])
+
+#    print(matricies[0].matrix)
+
+    for g in linkedObjects[0]:
+        print(g.vid, ": ")
+        for i in g.adjList:
+            print(i.vid)
+
+
 
 
 main()
