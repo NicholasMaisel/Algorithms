5bd2c9f49c9d61b6db13d8704de553a05b84a5b9
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-04-16 17:24:38 -0400

    Minor changes

--- graphMatrix.py
+++ graphMatrix.py
@@ -1,4 +1,4 @@
-from vertex import Vertex
+
 
 class GraphMatrix:
     def __init__(self):
@@ -16,4 +16,4 @@ class GraphMatrix:
         for vert in adjacencyList:  # Loops through each vertex
             for connection in adjacencyList[vert]:  # Loops through each adj vertex
                 self.matrix[int(vert)-1][int(connection)-1] = 1  # Updates the matrix with a connection
-                self.matrix[int(connection)-1][int(vert)-1] = 1     
+                self.matrix[int(connection)-1][int(vert)-1] = 1
