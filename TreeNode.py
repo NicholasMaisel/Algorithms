b22030858fde1cafe6a87d1e9cb5bb1f1d09ff0a
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-04-17 20:56:38 -0400

    Removes unused code

--- TreeNode.py
+++ TreeNode.py
@@ -3,12 +3,3 @@ class TreeNode:
         self.vid = vid
         self.left = None
         self.right = None
-
-
-    def getChildren(self):
-        children = []
-        if self.left != None:
-            children.append(self.left)
-        if self.right != None:
-            children.append(self.right)
-        return(children)
